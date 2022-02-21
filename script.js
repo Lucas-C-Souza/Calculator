081101e1653d0c46b3bc7329eb0435c4be7d5bd4
Lucas da Costa Souza <luc.costa.souza@gmail.com>
2022-02-21 11:25:56 -0300

    first Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function calcular(tipo, valor) {
+  if (tipo === 'acao') {
+    if (valor === 'c') {
+      document.getElementById('resultado').value = ''
+    }
+
+    if (
+      valor === '+' ||
+      valor === '-' ||
+      valor === '*' ||
+      valor === '/' ||
+      valor === '.'
+    ) {
+      document.getElementById('resultado').value += valor
+    }
+
+    if (valor === '=') {
+      var valor_campo = eval(document.getElementById('resultado').value)
+
+      document.getElementById('resultado').value = valor_campo
+    }
+  } else if (tipo === 'valor') {
+    document.getElementById('resultado').value += valor
+  }
+}
